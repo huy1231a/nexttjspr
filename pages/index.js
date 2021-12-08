481b130e1f827fa0494f11d850eef37414b1b4b7
huy1231a <taolahuy2k0@gmail.com>
2021-12-08 12:04:43 +0700

    con res em chua lam xong nah a

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 import React from 'react'
 import About from './about'
 import Link from 'next/link'
- 
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
 	return (
@@ -17,12 +17,12 @@ export default function Home() {
 					<p><a href="#">The New Gym coLtd</a></p>
 					<ul>
 						<li><Link href="/about">About</Link></li>
-						<li><a href="#">Features</a></li>
+						<li><a href="#" > <FontAwesomeIcon icon={["fal", "coffee"]} /> Features</a></li>
 						<li><a href="#">Use Case</a></li>
 						<li><a href="#">Pricing</a></li>
 
 					</ul>
-
+					<FontAwesomeIcon className="bar" icon={faBars}></FontAwesomeIcon>
 				</header>
 				<div className="contenido">
 					<div className="info">
